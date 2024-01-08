ddeb4652e5d30b92cccf4f9f9dcced71bac62c1b
MaestroOG <muneeburrehman822@gmail.com>
2024-01-08 18:38:52 +0500

    changed the whole logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,43 @@
-const searchBox = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search button");
-const thalaHeading = document.querySelector(".thala h1");
+const inputValue = document.querySelector("input");
+const btn = document.querySelector("button");
 
-function thalaNot() {
-    if (searchBox.value.length === 7) {
-        thalaHeading.style.display = "block";
-        var thalaBgm = new Audio("images/thalabgm.mp3");
-        thalaBgm.play();
+function getValue() {
+    if (!isNaN(inputValue.value)) {
+        console.log('Numeric value entered');
+        sumofDigits(inputValue.value);
     } else {
-        thalaHeading.style.display = "none";
+        console.log('String value entered');
+        thalaOrNot(inputValue.value.length);
     }
 }
 
-searchBtn.addEventListener('click', thalaNot);
+function sumofDigits(num) {
+    let numString = num.toString();
+    let sum = 0;
+
+    for (let i = 0; i < numString.length; i++) {
+        sum += parseInt(numString[i]);
+    }
+    thalaOrNot(sum);
+}
+
+
+function thalaOrNot(sum) {
+    if (sum === 7) {
+        console.log("Thala For A Reason!");
+        var audio = new Audio("images/thalabgm.mp3");
+        audio.play();
+        setTimeout(() => {
+            audio.pause();
+        }, 10000);
+    } else {
+        console.log("Mera Lund Jiye Aisi Zindagi");
+    }
+}
+
+document.addEventListener("keydown", (event) => {
+    let name = event.key;
+    if (name === "Enter") {
+        getValue();
+    }
+})
